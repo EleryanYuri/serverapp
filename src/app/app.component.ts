9dd4b36793b8b2bd8c6985459068b468502e8118
aurelien <aurelien@pop-os.localdomain>
2022-05-28 20:32:57 +0200

    test backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,21 @@ import { ServerService } from './service/server.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   appState$!: Observable<AppState<CustomResponse>>;
   constructor(private serverService: ServerService) {}
 
   ngOnInit(): void {
-    this.appState$ = this.serverService.servers$
-    .pipe(
-      map(response => {
-        return { dataState: DataState.LOADED_STATE, appData: response }
+    this.appState$ = this.serverService.servers$.pipe(
+      map((response) => {
+        return { dataState: DataState.LOADED_STATE, appData: response };
       }),
       startWith({ dataState: DataState.LOADING_STATE }),
-      catchError((error: string)=> {
-        return of({ dataState: DataState.ERROR_STATE, error: error })
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error: error });
       })
-    )
+    );
   }
 }
